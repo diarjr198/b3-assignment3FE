8bb52f8912aa46e86689c8a803cb3ebae5a9f166
Diar J R <diar.student@gmail.com>
2021-12-19 20:20:28 +0700

    testing-frontend

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 const AUTH_API = environment.API_URL + 'api/auth/';
 
--- src/app/_services/cart.service.ts
+++ src/app/_services/cart.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { Cart } from '../_models/Cart';
 import { Product } from '../_models/Product';
 import { TokenStorageService } from './token-storage.service';
